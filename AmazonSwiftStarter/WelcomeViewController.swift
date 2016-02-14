ccab75f9b092680af06a8cf744d5de5736832c54
Peter Fennema <peterfennema@users.noreply.github.com>
2016-02-14 21:46:23 +0100

    Background image for home screen UI improvements WelcomeVC now has 3 states

--- AmazonSwiftStarter/WelcomeViewController.swift
+++ AmazonSwiftStarter/WelcomeViewController.swift
@@ -16,10 +16,55 @@ protocol WelcomeViewControllerDelegate: class {
 
 class WelcomeViewController: UIViewController {
     
+    enum State {
+        case Welcome
+        case Welcomed
+        case Error
+    }
+
+
     weak var delegate: WelcomeViewControllerDelegate?
     
+    private var state: State = .Welcome {
+        didSet {
+            switch state {
+            case .Welcome:
+                explanationLabel.text = "Anonymous Sign In wil call AWS Cognito. The app will receive an identity token from the Cognito service and will store it on the device."
+                errorLabel.hidden = true
+                signInButton.hidden = false
+                createProfileButton.hidden = true
+            case .Welcomed:
+                explanationLabel.text = "You are now signed in. An empty user profile with a unique userId has already been created in DynamoDB. In the next step the user can add his username and a profile image. The username will be added to DynamoDB, the image will be uploaded to an S3 Bucket"
+                errorLabel.hidden = true
+                signInButton.hidden = true
+                createProfileButton.hidden = false
+            case .Error:
+                errorLabel.hidden = false
+            }
+        }
+    }
+    
+
+    @IBOutlet weak var explanationLabel: UILabel!
+    
+    @IBOutlet weak var errorLabel: ErrorLabel!
+    
+    @IBOutlet weak var signInButton: UIButton!
+    
+    @IBOutlet weak var createProfileButton: UIButton!
+    
+    
+    @IBAction func didTapSignInButton(sender: UIButton) {
+        state = .Welcomed
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        state = .Welcome
+    }
+    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if segue.identifier == "startSegue" {
+        if segue.identifier == "editProfileSegue" {
             let destVC = segue.destinationViewController as! EditProfileViewController
             destVC.delegate = self
         }
